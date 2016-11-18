d8e0bcda98790f3e0ea944968a0e034a5ae02312
ilievv <dmxyyyy@gmail.com>
2016-11-18 17:23:05 +0200

    refactoring

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-require("./utils/simple-movie-scrapper.js");
+const simpleMovieScrapper = require("./utils/simple-movie-scrapper");
+const constants = require("./config/constants");
+const queuesFactory = require("./data-structures/queue");
+
+require("./config/mongoose")(constants.connectionString);
+
+let urlsQueue = queuesFactory.getQueue();
+
+constants.genres.forEach(genre => {
+    for (let i = 0; i < constants.pagesCount; i += 1) {
+        let url = constants.searchUrl({ genre: genre, page: i + 1 });
+        urlsQueue.push(url);
+    }
+});
+
+Array.from({ length: constants.asyncPagesCount })
+    .forEach(() => simpleMovieScrapper.getMoviesFromUrl(urlsQueue.pop()))
--- utils/simple-movie-scrapper.js
+++ utils/simple-movie-scrapper.js
@@ -5,19 +5,7 @@ const httpRequester = require("./http-requester");
 const htmlParser = require("./html-parser");
 const queuesFactory = require("../data-structures/queue");
 const modelsFactory = require("../models");
-const constants = require("../config/constants")
 const utilities = require("./utilities");
-
-require("../config/mongoose")(constants.connectionString);
-
-let urlsQueue = queuesFactory.getQueue();
-
-constants.genres.forEach(genre => {
-    for (let i = 0; i < constants.pagesCount; i += 1) {
-        let url = constants.searchUrl({ genre: genre, page: i + 1 });
-        urlsQueue.push(url);
-    }
-});
 
 const getMoviesFromUrl = function(url) {
     console.log(`Working with ${url}`);
@@ -47,9 +35,6 @@ const getMoviesFromUrl = function(url) {
         .catch((err) => {
             console.dir(err, { colors: true });
         });
-}
-
-Array.from({ length: constants.asyncPagesCount })
-    .forEach(() => getMoviesFromUrl(urlsQueue.pop()));
+};
 
 module.exports.getMoviesFromUrl = getMoviesFromUrl;
